ca0ab699eaff129a73d191b33e6fac0e366aff4b
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-12 16:07:36 -0400

    some chanes in momgo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { addAFish, getAllFish } from "./src/fish.js";
+import { addAFish, getAllFish, deleteOneFish, updateOneFish } from "./src/fish.js";
 import { closeCliet } from "./src/connectDb.js";
 
 await addAFish();
 await getAllFish();
+await deleteOneFish();
+await updateOneFish();
 await closeCliet();
--- src/fish.js
+++ src/fish.js
@@ -1,16 +1,24 @@
+import { ObjectId } from "mongodb";
 import { db } from "./connectDb.js";
+const fishDb = db.collection('fish')
 
 const theFish = {
-    species: 'tetra',
+    species: 'goldfish',
     orgin: 'Amazon',
     color: 'silever',
     size: '1"',
+    famliy: {dad: '🐠',
+    sister: '🍣',
+    mom:{cool: true, special: undefined},
+    brothers: ['🐡', '🐟']
+},
 
 };
 // add a fish
 
 export async function addAFish(){
-await db.collection('fish').insertOne(theFish);
+const fishAdded = await db.collection('fish').insertOne(theFish);
+console.log('fishAdded ->', fishAdded);
 }
 
 // get all fishies
@@ -18,4 +26,31 @@ await db.collection('fish').insertOne(theFish);
     const fishList =  await db.collection('fish').find({}).toArray();
     console.table(fishList);
  }
- 
+
+ // delete goldfish ???
+ export async function deleteOneFish() {
+// 1 find the fish we want to delete ??
+// 2 delete it !!
+
+//const fishDeleted = await db.collection('fish').findOneAndDelete({species: 'goldfish'});
+
+// find one and delete by id??? wtf??
+ const fishId = new ObjectId('65284caea76561f7fc303647')
+const fishDeleted = await db.collection('fish').findOneAndDelete({_id: fishId})
+console.log('fishDeleted ->', fishDeleted);
+
+ }
+
+// update fish 
+
+export async function updateOneFish() {
+// 1 define the fish we want to update 
+// 2 determine what property we want to update 
+// 3 find the item we wan update 
+// 4 then we update with new data
+ const cleanId = new ObjectId('65284835ae3886151fb772c8')
+ const dataToUpdate = { color: 'red'}
+const updatedFish = await fishDb.findOneAndUpdate({_id: cleanId }, {$set:dataToUpdate})
+console.log('updateFish ->', updatedFish)
+
+}
